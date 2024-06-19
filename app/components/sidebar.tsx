dc10593d362c9995f961546d1d56452245186ddc
rahul <aarjayjangir@gmail.com>
2024-06-19 11:49:13 +0530

    chnage color theme

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           aarjaycreation
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Rahul Personal ai interface
+          Rahul Personal AI Interface.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
